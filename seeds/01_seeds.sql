5550defb2aacb9d9b5a8df2038836d2dc8cb7148
amusictheorist <a_millaresthomson@outlook.com>
2024-10-16 18:04:17 -0400

    Fix small bug in table population

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,19 +1,19 @@
-INSERT INTO users (id, name, email, password)
-VALUES (1, 'Geddy Lee', 'geddy.lee@rush.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-(2, 'Alex Lifeson', 'alex.lifeson@rush.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-(3, 'Neil Peart', 'neil.peart@rush.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password)
+VALUES ('Geddy Lee', 'geddy.lee@rush.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Alex Lifeson', 'alex.lifeson@rush.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Neil Peart', 'neil.peart@rush.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO  properties (id, owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-VALUES (1, 2, 'title', 'description', 'url', 'url', 31, 0, 1, 1, 'Canada', 'Raglan Avenue', 'Toronto', 'ON', 'M6C 2L3', true),
-(2, 3, 'title', 'description', 'url', 'url', 37, 1, 2, 3, 'Canada', 'Osler Street', 'Toronto', 'ON', 'M6N 2Y8', true),
-(3, 1, 'title', 'description', 'url', 'url', 41, 2, 2, 4, 'Canada', 'Glenside Avenue', 'Toronto', 'ON', 'M4L 2T6', true);
+INSERT INTO  properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (2, 'title', 'description', 'url', 'url', 31, 0, 1, 1, 'Canada', 'Raglan Avenue', 'Toronto', 'ON', 'M6C 2L3', true),
+(3, 'title', 'description', 'url', 'url', 37, 1, 2, 3, 'Canada', 'Osler Street', 'Toronto', 'ON', 'M6N 2Y8', true),
+(1, 'title', 'description', 'url', 'url', 41, 2, 2, 4, 'Canada', 'Glenside Avenue', 'Toronto', 'ON', 'M4L 2T6', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2018-09-11', '2018-09-26', 1, 1),
 ('2019-01-04', '2019-02-01', 2, 2),
 ('2021-10-01', '2021-10-14', 3, 3);
 
-INSERT INTO property_reviews (id, guest_id, property_id, reservation_id, rating, message)
-VALUES (1, 3, 2, 1, 5, 'message'),
-(2, 1, 3, 1, 4, 'message'),
-(3, 2, 1, 2, 3, 'message');
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (3, 2, 1, 5, 'message'),
+(1, 3, 1, 4, 'message'),
+(2, 1, 2, 3, 'message');
