4b4a999e9a90f44ceea665a988f9acc9e43d8ab4
amusictheorist <a_millaresthomson@outlook.com>
2024-10-17 16:16:58 -0400

    Refactor getAllProperties to include multiple parameter filtering

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -106,15 +106,48 @@ const getAllReservations = function(guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  const queryParams = [];
+
+  let queryString = `
+      SELECT properties.*, avg(propery_reviews.rating) as average_rating
+      FROM properties
+      JOIN property_reviews ON properties.id = property_id
+      `;
+
+      const conditions = [];
+      if (options.city) {
+        queryParams.push(`%${options.city}`);
+        conditions.push(`city LIKE $${queryParams.length}`);
+      }
+      if (options.owner_id) {
+        queryParams.push(options.owner_id);
+        conditions.push(`owner_id = $${queryParams.length}`);
+      }
+      if (options.minimum_price_per_night) {
+        queryParams.push(options.minimum_price_per_night);
+        conditions.push(`minimum_price_per_night = $${queryParams.length}`);
+      }
+      if (options.maximum_price_per_night) {
+        queryParams.push(options.maximum_price_per_night);
+        conditions.push(`maximum_price_per_night = $${queryParams.length}`);
+      }
+      if (options.minimum_rating) {
+        queryParams.push(options.minimum_rating);
+        conditions.push(`minimum_rating = $${queryParams.length}`);
+      }
+      if (conditions.length > 0) {
+        queryString += "WHERE " + conditions.join(" AND ") + "\n";
+      }
+
+      queryParams.push(limit);
+      queryString += `
+      GROUP BY properties.id
+      ORDER BY cost_per_night
+      LIMIT $${queryParams.length}
+      `;
+      
+      console.log(queryString, queryParams);
+      return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
 /**
